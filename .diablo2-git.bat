1915a1421b48cd83b12efb4860ccef946ce72c19
Gustavo Henrique Pinto <ghp2201@gmail.com>
2020-08-17 20:09:16 -0300

    Add files via upload

---
+++ .diablo2-git.bat
@@ -0,0 +1,7 @@
+@echo off
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
+
+git add .
+git commit -m "%mydate%_%mytime%"
+git push
