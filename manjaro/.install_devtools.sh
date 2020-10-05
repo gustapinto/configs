89a6285b60bdc3481b12420f5a4cdb2357219964
Gustavo Pinto <gustavo.pinto@cassum.dev>
2020-10-05 18:12:48 -0300

    Reformulate basic install script

--- manjaro/.install_devtools.sh
+++ manjaro/.install_devtools.sh
@@ -4,12 +4,18 @@
 sudo pacman -S base-devel --noconfirm
 sudo pacman -S yay --noconfirm
 sudo pacman -S vim --noconfirm
+sudo pacman -S flameshot --noconfirm
 sudo pacman -S docker --noconfirm
 sudo pacman -S docker-compose --noconfirm
+sudo pacman -S dbeaver --noconfirm
+sudo pacman -S firefox-developer-edition-i18n-pt-br --noconfirm
+sudo pacman -S gufw --noconfirm
+
+yay -S visual-studio-code-bin --noconfirm
 yay -S spotify --noconfirm
-yay -S google-chrome --noconfirm
 yay -S discord --noconfirm
 
+#atualizando sistema
 sudo pacman -Syyu --noconfirm
 yay -Syyu --noconfirm
 
