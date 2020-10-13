c6533d407838355f3ad5d74f16ec1cea498f093b
ghp2201 <ghp2201@gmail.com>
2020-10-13 09:01:46 -0300

    Added missing install flatpak

---
+++ ubuntu/install_flatpak.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install flatpak
+
+sudo apt install gnome-software
+sudo apt install gnome-software-plugin-flatpak
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
