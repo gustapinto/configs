e415e30929e73599d721489baa6a7b5dad46e935
Gustavo Henrique Pinto <ghp2201@gmail.com>
2020-11-07 09:23:59 -0300

    Update pop_os_post_installation.sh

--- pop_os_post_installation.sh
+++ pop_os_post_installation.sh
@@ -5,19 +5,17 @@ sudo apt update -y
 sudo apt upgrade -y
 flatpak update -y
 
+# Install apt based apps
+sudo apt install os-prober docker docker-compose -y
+
 # Check for another OS on system and active dual boot if necessary
-sudo apt install os-prober -y
+sudo apt install  -y
 sudo os-prober
 sudo update-grub
 
-# Install apt based apps
-sudo apt install docker docker-compose -y
-
 # Install flatpak based apps and crete aliases for them
-flatpak install flathub org.vim.Vim com.visualstudio.code com.discordapp.Discord com.spotify.Client org.libreoffice.LibreOffice io.dbeaver.DBeaverCommunity -y
+flatpak install flathub com.sublimetext.three com.discordapp.Discord com.spotify.Client org.libreoffice.LibreOffice io.dbeaver.DBeaverCommunity -y
 
-echo "alias code='flatpak run com.visualstudio.code'" >> ~/.bashrc
-echo "alias vim='flatpak run org.vim.Vim'" >> ~/.bashrc
 source ~/.bashrc
 
 # Manage apps users and permissions
@@ -25,8 +23,7 @@ sudo usermod -aG docker $USER
 newgrp docker
 
 # Remove bloatware or duplicated apps
-sudo apt remove --purge libreoffice-core -y
-sudo apt remove --purge geary -y
+sudo apt remove --purge libreoffice-core geary -y
 
 # Remove unnecessary dependencies
 sudo apt autoclean -y
@@ -35,9 +32,6 @@ flatpak uninstall --unused -y
 
 # Enable firewall
 sudo ufw enable
-
-# Configure git to use flatpak vim as editor
-git config --global core.editor "flatpak run org.vim.Vim"
 
 # To enable Wayland compositor comment the WaylandEnable=false line
 sudo nano /etc/gdm3/custom.conf
