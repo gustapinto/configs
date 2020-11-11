a7b239f15cc8d4857ae5cb6713b2a81a568e64b7
Gustavo Henrique Pinto <ghp2201@gmail.com>
2020-11-11 08:36:12 -0300

    Update install.sh

--- sublime_text_flatpak/install.sh
+++ sublime_text_flatpak/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Copy the origin User settings to flatpak app directory
-cp -r ./User ~/.var/app/com.sublimetext.three/config/sublime-text-3/Packages/
+# Copy the repo User settings to Sublime app directory
+cp -R -v ./User ~/.var/app/com.sublimetext.three/config/sublime-text-3/Packages/
