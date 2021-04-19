fbb06d2a346eff01c471c4c94aacde8e85bb4755
Gustavo Henrique Pinto <ghp2201@gmail.com>
2021-04-18 21:43:10 -0300

    Update fedora script

--- fedora.sh
+++ fedora.sh
@@ -8,17 +8,20 @@ sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfre
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
-# Update dnf cache
-sudo dnf check-update
-
-# Install RPM based packages
-sudo dnf install steam code docker docker-compose gnome-tweaks gnome-extensions-app -y
-
-# Install GNOME extensions
-sudo dnf install gnome-shell-extension-pop-shell -y
-
 # Enable Flathub repository
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+# Update dnf cache and packages
+sudo dnf check-update -y
+sudo dnf upgrade -y
+
+# Install RPM based packages
+sudo dnf install steam code docker docker-compose gnome-tweaks -y
+
 # Install flatpak based packages
 flatpak install flathub md.obsidian.Obsidian com.spotify.Client -y
+
+# Enable docker groups
+sudo usermod -a -G docker $USER
+
+newgrp docker
