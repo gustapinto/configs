2b0a4a35cceaf3378c1dffa25b014e42e319a2f7
Gustavo Henrique Pinto <ghp2201@gmail.com>
2021-06-09 18:22:03 -0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -1,43 +1,51 @@
 #!bin/bash
 
 # Enable RPM Fusion repositories
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install -y \
+    https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# Enable VS Code repository
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-
-# Enable Flathub repository
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-# Enable docker-ce repository
-sudo dnf install dnf-plugins-core
-
+# Enable third party repositories
+sudo dnf install -y dnf-plugins-core
+sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Update dnf cache and packages
 sudo dnf check-update -y
 sudo dnf upgrade -y
 
 # Install RPM based packages
-sudo dnf install steam code gnome-tweaks -y
+sudo dnf install \
+    containerd.io \
+    docker-ce \
+    docker-ce-cli \
+    docker-compose \
+    gnome-tweaks \
+    steam \
+    sublime-text
 
 # Install flatpak based packages
-flatpak install flathub md.obsidian.Obsidian -y
+flatpak install \
+    com.discordapp.Discord \
+    flathub md.obsidian.Obsidian \
+    org.mozilla.firefox
 
-# Install docker-ce and docker-compose
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose -y
+# Remove duplicated packages
+sudo dnf remove -y \
+    firefox
 
-sudo grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"  # Set CGroup to version 1
+# Remove possible unused packages
+sudo dnf autoremove -y
+flatpak remove --unused -y
 
+# Set CGroup to version 1
+sudo grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
+
+# Enable and set docker-ce permissions
 sudo systemctl start docker
-sudo systemctl enable docker
-
 sudo groupadd docker
-
 sudo gpasswd -a ${USER} docker
-
 sudo systemctl restart docker
-
 newgrp docker
