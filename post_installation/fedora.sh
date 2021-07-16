b7efc0a96e69a28fe888bb15956d12be345b6986
Gustavo Henrique Pinto <ghp2201@gmail.com>
2021-07-15 22:45:42 -0300

    Update fedora script

--- post_installation/fedora.sh
+++ post_installation/fedora.sh
@@ -9,7 +9,6 @@ sudo dnf install -y \
 sudo dnf install -y dnf-plugins-core
 sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
 sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Update dnf cache and packages
@@ -17,35 +16,29 @@ sudo dnf check-update -y
 sudo dnf upgrade -y
 
 # Install RPM based packages
-sudo dnf install \
-    containerd.io \
-    docker-ce \
-    docker-ce-cli \
+sudo dnf install -y \
     docker-compose \
     gnome-tweaks \
-    steam \
+    podman\
     sublime-text
 
 # Install flatpak based packages
-flatpak install \
-    com.discordapp.Discord \
-    flathub md.obsidian.Obsidian \
-    org.mozilla.firefox
+flatpak install -y \
+    flathub md.obsidian.Obsidian
 
-# Remove duplicated packages
+# Remove Fedora bloat
 sudo dnf remove -y \
-    firefox
+    cheese \
+    gnome-boxes \
+    gnome-clocks \
+    gnome-contacts \
+    gnome-maps \
+    gnome-tour \
+    gnome-weather \
+    libreoffice-* \
+    simple-scan \
+    yelp
 
 # Remove possible unused packages
 sudo dnf autoremove -y
 flatpak remove --unused -y
-
-# Set CGroup to version 1
-sudo grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
-
-# Enable and set docker-ce permissions
-sudo systemctl start docker
-sudo groupadd docker
-sudo gpasswd -a ${USER} docker
-sudo systemctl restart docker
-newgrp docker
