92aa6dd6f402c8dc0160b8108bdc73c66b78c9a1
Gustavo Henrique Pinto <ghp2201@gmail.com>
2021-07-16 22:11:51 -0300

    Switch frompodman to docker-ce on fedora

--- post_installation/fedora.sh
+++ post_installation/fedora.sh
@@ -9,6 +9,7 @@ sudo dnf install -y \
 sudo dnf install -y dnf-plugins-core
 sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
 sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Update dnf cache and packages
@@ -17,9 +18,11 @@ sudo dnf upgrade -y
 
 # Install RPM based packages
 sudo dnf install -y \
+    containerd.io \
+    docker-ce \
+    docker-ce-cli \
     docker-compose \
     gnome-tweaks \
-    podman\
     sublime-text
 
 # Install flatpak based packages
@@ -42,3 +45,13 @@ sudo dnf remove -y \
 # Remove possible unused packages
 sudo dnf autoremove -y
 flatpak remove --unused -y
+
+# Set CGroup to version 1
+sudo grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
+
+# Enable and set docker-ce permissions
+sudo systemctl start docker
+sudo groupadd docker
+sudo gpasswd -a ${USER} docker
+sudo systemctl restart docker
+newgrp docker
