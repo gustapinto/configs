31de27f5ce6b4fe6f8e4ca4f2b7fc6318ac4dd29
Gustavo Henrique Pinto <ghp2201@gmail.com>
2022-08-14 11:58:02 -0300

    Add ubuntu script

---
+++ ubuntu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Add external repositories
+sudo add-apt-repository ppa:mmstick76/alacritty
+
+# Install software and prerequesites
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y \
+    alacritty \
+    docker \
+    docker-compose \
+    tmux \
+    vim \
+    zsh
+
+# Install .deb based software
+wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -O /tmp/vscode.deb && sudo dpkg -i /tmp/vscode.deb
+wget "https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb" -O /tmp/dbeaver.deb && sudo dpkg -i /tmp/dbeaver.deb
+wget "https://github.com/obsidianmd/obsidian-releases/releases/download/v0.15.9/obsidian_0.15.9_amd64.deb" -O /tmp/obsidian.deb && sudo dpkg -i /tmp/obsidian.deb
+
+# Install Oh My Zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# Configure docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo usermod -aG docker $USER
+newgrp docker
+
+# Configure dotfiles
+cp .alacritty.yml ~/.alacritty.yml
+cp .tmux.conf ~/.tmux.conf
+cp .vimrc ~/.vimrc
+cp .zsh_aliases ~/.zsh_aliases && echo "source ~/.zsh_aliases" >> ~/.zshrc
