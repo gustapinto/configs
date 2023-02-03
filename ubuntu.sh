2fa47386e596c4abde782cea9d8b73c0706c473a
Gustavo Henrique Pinto <ghp2201@gmail.com>
2023-02-02 22:41:04 -0300

    feat: update ubuntu script

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Add external repositories
-sudo add-apt-repository ppa:aslatter/ppa
+sudo apt install -y software-properties-common
+sudo add-apt-repository -y ppa:aslatter/ppa
 
 # Install software and prerequesites
-sudo apt update && sudo apt upgrade -y
+sudo apt update
 
 sudo apt install -y \
     alacritty \
@@ -13,28 +14,31 @@ sudo apt install -y \
     docker-compose \
     tmux \
     vim \
-    zsh
+    zsh \
+    fonts-liberation \
+    gnome-tweaks
 
-# Install .deb based software
-wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -O /tmp/vscode.deb && sudo dpkg -i /tmp/vscode.deb
-wget "https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb" -O /tmp/dbeaver.deb && sudo dpkg -i /tmp/dbeaver.deb
-wget "https://github.com/obsidianmd/obsidian-releases/releases/download/v0.15.9/obsidian_0.15.9_amd64.deb" -O /tmp/obsidian.deb && sudo dpkg -i /tmp/obsidian.deb
+# Install external installer based software
+wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -O /tmp/vscode.deb && \
+    sudo dpkg -i /tmp/vscode.deb
+wget "https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb" -O /tmp/dbeaver.deb && \
+    sudo dpkg -i /tmp/dbeaver.deb
+wget "https://github.com/obsidianmd/obsidian-releases/releases/download/v1.1.9/obsidian_1.1.9_amd64.deb" -O /tmp/obsidian.deb && \
+    sudo dpkg -i /tmp/obsidian.deb
+wget "https://updates.insomnia.rest/downloads/ubuntu/latest?&app=com.insomnia.app&source=website" -O /tmp/insomnia.deb && \
+    sudo dpkg -i /tmp/insomnia.deb
 
 # Install Oh My Zsh
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
-# Install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Cleanup
+sudo apt autoremove
 
 # Configure docker
-sudo systemctl start docker
-sudo systemctl enable docker
-
-sudo usermod -aG docker $USER
-newgrp docker
+sudo systemctl start docker && \
+    sudo systemctl enable docker && \
+    sudo usermod -aG docker $USER && \
+    newgrp docker
 
 # Configure dotfiles
-cp .alacritty.yml ~/.alacritty.yml
-cp .tmux.conf ~/.tmux.conf
-cp .vimrc ~/.vimrc
-cp .zsh_aliases ~/.zsh_aliases && echo "source ~/.zsh_aliases" >> ~/.zshrc
+cp ./.zshrc ~/.zshrc
